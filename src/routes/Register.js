c5d2a3ecfd798c9c6bad2a0ba55bb37d4d6fabe8
Farook Basha <farookbashamulla269@gmail.com>
2025-01-02 22:32:11 +0530

    final commit

--- src/routes/Register.js
+++ src/routes/Register.js
@@ -11,7 +11,7 @@ function Register() {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/register", {
+      const response = await fetch("https://calendar-app-2-zh8j.onrender.com/api/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password, role: "user" }), // Default role is "user"
